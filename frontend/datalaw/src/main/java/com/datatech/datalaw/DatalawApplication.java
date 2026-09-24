15cd35bd956512c3ee2a9af1f769e66900a96918
Jackson <jacksontadeu@hotmail.com>
2026-09-24 00:22:21 -0300

    SCRUM-90 :sparkles: feat: Inicial frontend project

---
+++ frontend/datalaw/src/main/java/com/datatech/datalaw/DatalawApplication.java
@@ -0,0 +1,13 @@
+package com.datatech.datalaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatalawApplication.class, args);
+	}
+
+}
